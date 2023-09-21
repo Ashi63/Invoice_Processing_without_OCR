4bb4ad6b17c91573c7facdcfd70c52d47b20ddcd
Ashi63 <ashish.ashi6388@gmail.com>
2023-09-21 17:16:39 +0530

    second commit

--- app_main.py
+++ app_main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import time
 import json
+import os
 from PIL import Image
 from utils.text_getter import generate_result
+from utils.list_of_fileds_question import fields_questions_list
 
 # setting up the side bar for image upload.
 st.sidebar.title('Upload Invoice Image')
@@ -37,66 +39,36 @@ if uploaded_image is not None:
     # setting up the form in sidebar for feilds to be extracted.
     with st.form(key ='Form1'):
         with st.sidebar:
-            checkbox_invoice = st.sidebar.checkbox('Invoice Number')
-            checkbox_date_of_issue = st.sidebar.checkbox('Date of issue')
-            checkbox_iban_code = st.sidebar.checkbox('IBAN code')
-            checkbox_net_worth = st.sidebar.checkbox('Total Net Worth')
-            checkbox_gross_worth = st.sidebar.checkbox('Total Gross Worth')
+            for field,question in fields_questions_list.items():
+                st.sidebar.checkbox(field)
             submit = st.form_submit_button(label='Extract Fields',type='primary')
+            
         if submit:
             col2.subheader('Extracted Details')
             col2.markdown('-----------')
-            if checkbox_invoice:
-                user_question = 'What is the Invoice Number?'
-                answer = generate_result(user_question,image)
-                col2.text_input('Invoice Number',value=answer)
-                extracted_data['Invoice Number'] = answer
-                
-            if checkbox_date_of_issue:
-                user_question = 'What is the date of issue?'
-                answer = generate_result(user_question, image)
-                col2.text_input('Date of Issue',value=answer)
-                extracted_data['Date of Issue'] = answer
-                
-            if checkbox_iban_code:
-                user_question = 'What is the IBAN:?'
-                answer = generate_result(user_question, image)
-                col2.text_input('IBAN',value=str.upper(answer))
-                extracted_data['IBAN'] = str.upper(answer)
-                
-            if checkbox_net_worth:
-                user_question = 'What is the Total Net worth?'
-                answer = generate_result(user_question, image)
-                col2.text_input('Net Worth',value=answer)
-                extracted_data['Net Worth'] = answer
-                
-            if checkbox_gross_worth:
-                user_question = 'What is the Total Gross worth?'
-                answer = generate_result(user_question, image)
-                col2.text_input('Gross Worth',value=answer)
-                extracted_data['Gross Worth'] = answer    
-                    
+
+            for field,question in fields_questions_list.items():
+                user_question = question
+                answer = str.upper(generate_result(user_question,image))
+                col2.text_input(field,value=answer)
+                extracted_data[field] = answer 
+                                
             # After the form submission, you can convert the extracted_data dictionary to JSON
             if extracted_data:
                 extracted_data_json = json.dumps(extracted_data, indent=4)
                 st.write("Extracted Data (JSON format):")
                 st.code(extracted_data_json)
             
-                # Save the JSON data to a file
+            # make extracted_data folder for saving the extracted data from image
+            if 'extracted_data' not in os.listdir():
+                os.makedirs('extracted_data',exist_ok=True)
+                
+            # Save the JSON data to a file
             with open('extracted_data/extracted_data.json', 'w') as json_file:
                 json.dump(extracted_data, json_file, indent=4)
 
-            st.success("Extracted data saved to 'extracted_data.json'")
-            
+            st.success("Extracted data saved to 'extracted_data.json'")    
 else:
     st.warning('Upload Image')   
 
-
-
-            
-
-
-        
-    
-
-        
+                
---
+++ utils/list_of_fileds_question.py
@@ -0,0 +1,6 @@
+fields_questions_list ={'Invoice Number':'What is the Invoice Number?',
+                       'Date of issue': 'What is the Date of issue?',
+                       'IBAN code': 'What is the IBAN:?',
+                       'Total Net Worth':'What is the Total Net worth?',
+                       'Total Gross Worth': 'What is the Total Gross worth?'
+                       }
